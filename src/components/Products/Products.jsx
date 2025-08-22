92c232b3998782128250c80c84ddb3d62e4f9ca9
Sazedur <sazedur11ab@gmail.com>
2025-08-23 00:03:13 +0600

    features

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,11 @@
+import { SlCup } from "react-icons/sl";
 import bgImg1 from "../../assets/more/4.png";
 import bgImg2 from "../../assets/more/5.png";
 
 const Products = () => {
   return (
       <div
-        className="py-50 bg-no-repeat bg-cover"
+        className="bg-no-repeat bg-cover"
         style={{
           backgroundImage: `url(${bgImg1}), url(${bgImg2})`,
           backgroundPosition: "top left, right bottom",
@@ -12,9 +13,10 @@ const Products = () => {
           backgroundSize: "200px 200px, contain",
         }}
       >
-        <div>
-            <p className="text-gray-900">--- Sip & Savor ---</p>
-            <h3 className="text-gray-900 text-5xl font-Rancho">Our Popular Products</h3>
+        <div className="w-4/5 mx-auto flex flex-col justify-center items-center gap-3">
+            <p className="text-gray-900 text-center">--- Sip & Savor ---</p>
+            <h3 className="text-gray-900 text-5xl font-Rancho text-center">Our Popular Products</h3>
+            <button className="bg-[#E3B577] border-2 border-[#331A15] text-white text-shadow-lg/30 hover:bg-white  hover:text-gray-900  rounded-sm font-Rancho px-4 py-1 flex items-center gap-2">Add Coffee <SlCup></SlCup> </button>
         </div>
       </div>
   );
