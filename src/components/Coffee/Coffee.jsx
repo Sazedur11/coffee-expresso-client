eeee8e8a0e44ee4552e3f306af3a6ccbc29fa733
Sazedur <sazedur11ab@gmail.com>
2025-08-24 23:24:12 +0600

    coffe Card

--- src/components/Coffee/Coffee.jsx
+++ src/components/Coffee/Coffee.jsx
@@ -1,11 +1,26 @@
+import { FaEye } from "react-icons/fa";
+import { MdDelete, MdEdit } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Coffee = ({coffee}) => {
 
     const { name, chef, supplier, taste, category, details, photo } = coffee;
 
     return (
-        <div>
-            <h2 className='text-green-500 font-bold text-4xl'> hello coffee {name}</h2>
+        <div className="flex justify-center items-center gap-4 bg-[#F5F4F1] p-4 rounded-xl">
+            <div>
+                <img src={photo} alt={name} />
+            </div>
+            <div className="text-gray-900">
+                <p className="font-bold">Name: <span className="font-normal">{name}</span></p>
+                <p className="font-bold">Chef: <span className="font-normal">{chef}</span></p>
+                <p className="font-bold">Taste: <span className="font-normal">{taste}</span></p>
+            </div>
+            <div className="text-white flex flex-col gap-2 mx-4">
+                <Link to={'/'} > <button  className="bg-[#D2B48C] p-2 rounded-sm"><FaEye></FaEye></button> </Link>
+                <Link to={'/'} > <button  className="bg-[#3C393B] p-2 rounded-sm"><MdEdit></MdEdit></button> </Link>
+                <Link to={'/'} > <button  className="bg-[#EA4744] p-2 rounded-sm"><MdDelete></MdDelete></button> </Link>
+            </div>
         </div>
     );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -13,7 +13,7 @@ const Products = ({ allCoffee }) => {
         backgroundImage: `url(${bgImg1})`,
       }}
     >
-      <div className="w-4/5 mx-auto flex flex-col justify-center items-center gap-3 py-50">
+      <div className="w-4/5 mx-auto flex flex-col justify-center items-center gap-3">
         <p className="text-gray-900 text-center">--- Sip & Savor ---</p>
         <h3 className="text-[#331A15] text-4xl font-Rancho text-center text-shadow-lg/30">
           Our Popular Products
@@ -23,7 +23,7 @@ const Products = ({ allCoffee }) => {
             Add Coffee <SlCup></SlCup>{" "}
           </button>
         </Link>
-        <div>
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4 mt-8">
           {allCoffee.map((coffee) => (
             <Coffee key={coffee._id} coffee={coffee}></Coffee>
           ))}
