c703d13630a4423eadd688800474d0007a1877ef
Sazedur <sazedur11ab@gmail.com>
2025-08-25 23:02:04 +0600

    updated coffee

--- src/components/Coffee/Coffee.jsx
+++ src/components/Coffee/Coffee.jsx
@@ -1,28 +1,79 @@
 import { FaEye } from "react-icons/fa";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
-const Coffee = ({coffee}) => {
+const Coffee = ({ coffee }) => {
+  const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
 
-    const { name, chef, supplier, taste, category, details, photo } = coffee;
+  const handleDelete = (_id) => {
+    
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
 
-    return (
-        <div className="flex justify-center items-center gap-4 bg-[#F5F4F1] p-4 rounded-xl">
-            <div>
-                <img src={photo} alt={name} />
-            </div>
-            <div className="text-gray-900">
-                <p className="font-bold">Name: <span className="font-normal">{name}</span></p>
-                <p className="font-bold">Chef: <span className="font-normal">{chef}</span></p>
-                <p className="font-bold">Taste: <span className="font-normal">{taste}</span></p>
-            </div>
-            <div className="text-white flex flex-col gap-2 mx-4">
-                <Link to={'/'} > <button  className="bg-[#D2B48C] p-2 rounded-sm"><FaEye></FaEye></button> </Link>
-                <Link to={'/'} > <button  className="bg-[#3C393B] p-2 rounded-sm"><MdEdit></MdEdit></button> </Link>
-                <Link to={'/'} > <button  className="bg-[#EA4744] p-2 rounded-sm"><MdDelete></MdDelete></button> </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center gap-4 bg-[#F5F4F1] p-4 rounded-xl">
+      <div>
+        <img className="w-[180px] h-[220px]" src={photo} alt={name} />
+      </div>
+      <div className="text-gray-900">
+        <p className="font-bold">
+          Name: <span className="font-normal">{name}</span>
+        </p>
+        <p className="font-bold">
+          Chef: <span className="font-normal">{chef}</span>
+        </p>
+        <p className="font-bold">
+          Taste: <span className="font-normal">{taste}</span>
+        </p>
+      </div>
+      <div className="text-white flex flex-col gap-2 mx-4">
+        <Link to={"/"}>
+          
+          <button className="bg-[#D2B48C] p-2 rounded-sm">
+            <FaEye></FaEye>
+          </button>
+        </Link>
+        <Link to={`/updateCoffee/${_id}`}>
+          
+          <button className="bg-[#3C393B] p-2 rounded-sm">
+            <MdEdit></MdEdit>
+          </button>
+        </Link>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="bg-[#EA4744] p-2 rounded-sm"
+        >
+          <MdDelete></MdDelete>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Coffee;
--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,10 +1,123 @@
+import { Link, useLoaderData } from "react-router-dom";
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import { FaArrowLeft } from "react-icons/fa";
 
 const UpdateCoffee = () => {
-    return (
-        <div>
-            <h2>Update coffee</h2>
+  const allCoffee = useLoaderData();
+  const { name, chef, supplier, taste, category, details, photo } = allCoffee;
+
+  const handleUpdatedCoffee = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="bg-white pt-10 pb-20">
+        <div className="w-4/5 mx-auto">
+          <Link
+            className="w-33 text-[#374151] text-shadow-lg/30 font-Rancho px-4 py-2 rounded-sm hover:bg-[#D2B48C] flex items-center gap-2"
+            to={"/"}
+          >
+            <FaArrowLeft></FaArrowLeft> Back to home
+          </Link>
+          <div className="bg-[#F4F3F0] px-10 py-15">
+            <h3 className="text-[#331A15] text-4xl font-Rancho text-center text-shadow-lg/30">
+              Update Existing Coffee Details
+            </h3>
+            <p className="text-gray-900 text-center w-2xl mx-auto my-4">
+              It is a long established fact that a reader will be distraceted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using Content here.
+            </p>
+            <form onSubmit={handleUpdatedCoffee}>
+              {/* form raw*/}
+              <div className="flex gap-4 mb-4">
+                <div className="flex flex-col gap-2 w-1/2">
+                  <label className="text-gray-900">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="w-full input bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                    placeholder="Coffee Name"
+                  />
+                </div>
+                <div className="flex flex-col gap-2 w-1/2">
+                  <label className="text-gray-900">Chef</label>
+                  <input
+                    type="text"
+                    name="chef"
+                    className="input w-full bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                    placeholder="Coffee Chef"
+                  />
+                </div>
+              </div>
+              {/* form raw*/}
+              <div className="flex gap-4 mb-4">
+                <div className="flex flex-col gap-2 w-1/2">
+                  <label className="text-gray-900">Supplier</label>
+                  <input
+                    type="text"
+                    name="supplier"
+                    className="w-full input bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                    placeholder="Supplier"
+                  />
+                </div>
+                <div className="flex flex-col gap-2 w-1/2">
+                  <label className="text-gray-900">Taste</label>
+                  <input
+                    type="text"
+                    name="taste"
+                    className="input w-full bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                    placeholder="Taste"
+                  />
+                </div>
+              </div>
+              {/* form raw*/}
+              <div className="flex gap-4 mb-4">
+                <div className="flex flex-col gap-2 w-1/2">
+                  <label className="text-gray-900">Category</label>
+                  <input
+                    type="text"
+                    name="category"
+                    className="w-full input bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                    placeholder="Category"
+                  />
+                </div>
+                <div className="flex flex-col gap-2 w-1/2">
+                  <label className="text-gray-900">Details</label>
+                  <input
+                    type="text"
+                    name="details"
+                    className="input w-full bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                    placeholder="Details"
+                  />
+                </div>
+              </div>
+              {/* form raw*/}
+              <div className="flex flex-col gap-2 mb-4">
+                <label className="text-gray-900">Photo Url</label>
+                <input
+                  type="text"
+                  name="photo"
+                  className="w-full input bg-white text-gray-600 focus:border focus:border-[#E3B577] focus:ring-2 focus:ring-[#E3B577] "
+                  placeholder="Photo Url"
+                />
+              </div>
+              <input
+                className="text-[#331A15] font-Rancho btn btn-block bg-[#D2B48C]"
+                type="submit"
+                value="Update Coffee Details"
+              />
+            </form>
+          </div>
         </div>
-    );
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default UpdateCoffee;
